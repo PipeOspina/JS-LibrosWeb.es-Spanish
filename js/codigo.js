34769743826441ab4f24a3b7c53dc4500ddc7c10
Pipe Ospina <pipe.98.oh@gmail.com>
2017-11-09 14:31:56 -0500

    Ejercicio 4: Operadores

--- js/codigo.js
+++ js/codigo.js
@@ -5,3 +5,22 @@ var meses = ["Enero", "Febreo", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Ago
 //Arroja cuadros de dialogo con sus respectivos mensajes
 alert(mensaje);
 alert("Soy el primer script :)");
+alert(meses);
+
+
+var valores = [true, 5, false, "hola", "adios", 2];
+var resultado;
+//Se compara "hola" y "adios", se compara letra por letra (a es menor que b menor que c) y las mayusculas son menores que las minusculas (A es menor que a y B es menor que a)
+resultado = valores[3] < valores[4];
+alert(resultado); // false
+
+resultado = valores[0] && valores[2];
+alert(resultado); // false
+resultado = valores[0] || valores[2];
+alert(resultado); // true
+
+resultado = valores[1] + valores[5]; // Suma = 7
+resultado = valores[1] - valores[5]; // Resta = 3
+resultado = valores[1] * valores[5]; // Multiplicación = 10
+resultado = valores[1] / valores[5]; // División = 2,5
+resultado = valores[1] % valores[5]; // Modulo = 1
