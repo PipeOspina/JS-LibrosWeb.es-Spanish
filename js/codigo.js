a6339693fb3ecc670d7f3bab4c30717e3ee52a08
Pipe Ospina <pipe.98.oh@gmail.com>
2017-11-09 14:17:37 -0500

    Ejercicio 2 y 3: Tipos de variables

--- js/codigo.js
+++ js/codigo.js
@@ -1,3 +1,7 @@
+var mensaje = "Hola mundo!\nQue facil es incluir \'Comillas Simples\'\ny \"Comillas Dobles\"";
+
+var meses = ["Enero", "Febreo", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
 //Arroja cuadros de dialogo con sus respectivos mensajes
-alert("Hola mundo!");
+alert(mensaje);
 alert("Soy el primer script :)");
